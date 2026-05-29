3a9579c3d2d4436e241f6acaca45798790236d60
Couraa0 <muhammadrakhasyamputra@gmail.com>
2026-05-29 23:27:32 +0700

    chore: update vercel output server directory path to index.func

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     preset: 'vercel',
     output: {
       dir: '.vercel/output',
-      serverDir: '.vercel/output/functions/__server.func',
+      serverDir: '.vercel/output/functions/index.func',
       publicDir: '.vercel/output/static'
     }
   },
